b461256f7fa5c2c80c6848d8d39bee575b76d5c1
BolleTito335409 <chemichaeldekraker@gmail.com>
2023-10-23 22:37:32 +0200

    config aanpassing

--- app/config/config.php
+++ app/config/config.php
@@ -4,4 +4,4 @@ define('DB_HOST',  'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'Be-opdracht-02');
-define('URLROOT', 'http://be-opdracht2');
+define('URLROOT', 'http://www.php-opdracht2.com');
